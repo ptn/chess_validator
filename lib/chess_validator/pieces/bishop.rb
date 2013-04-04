c91cf995629833443aacc7d8bf62910e981a1d75
Pablo Torres <tn.pablo@gmail.com>
2013-04-03 22:36:35 -0500

    Get rid of badly-named method

--- lib/chess_validator/pieces/bishop.rb
+++ lib/chess_validator/pieces/bishop.rb
@@ -12,21 +12,30 @@ module ChessValidator
         column_diff == row_diff
       end
 
-      def obstacles?(destination)
-        column_direction = @position[0].ord - destination[0].ord < 0 ? 1 : -1
-        row_direction = @position[1].to_i - destination[1].to_i < 0 ? 1 : -1
-        _obstacles? @position, destination, column_direction, row_direction
-      end
-
       # Recursively check if there are obstacles in the diagonal
       # connecting two squares.
-      def _obstacles?(current, destination, column_direction, row_direction)
+      def obstacles?(destination,
+                     current=@position,
+                     column_direction=nil,
+                     row_direction=nil)
+        column_direction = default_column_direction(destination) unless column_direction
+        row_direction = default_row_direction(destination) unless row_direction
+
         return false if current == destination
         return true unless current == @position || @board[current].nil?
+
         next_column = (current[0].ord + column_direction).chr
         next_row = (current[1].to_i + row_direction).to_s
         next_position = next_column + next_row
-        _obstacles? next_position, destination, column_direction, row_direction
+        obstacles? destination, next_position, column_direction, row_direction
+      end
+
+      def default_column_direction(destination)
+        @position[0].ord - destination[0].ord < 0 ? 1 : -1
+      end
+
+      def default_row_direction(destination)
+        @position[1].to_i - destination[1].to_i < 0 ? 1 : -1
       end
     end
   end
